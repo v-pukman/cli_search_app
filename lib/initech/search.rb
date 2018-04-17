e6c86bf41cb02e46d587b56bed52f9ff721339fc
Victor Pukman <victor.pukman@gmail.com>
2018-04-17 19:15:32 +0800

    start method added

--- lib/initech/search.rb
+++ lib/initech/search.rb
@@ -11,5 +11,14 @@ module Initech
     rescue JSON::ParserError => e
       logger.error e.message
     end
+
+    def search json_data, key, value
+      json_data.select {|i| i[key].to_s == value.to_s }
+    end
+
+    def start file_path, key, value
+      json_data = parse_file file_path
+      search json_data, key, value
+    end
   end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,6 +5,7 @@ require 'initech/search'
 RSpec.describe "Search" do
   let(:search) { Initech::Search.new }
   let(:file_path) { File.join('spec', 'fixtures', 'files', "users.json") }
+
   describe "#parse_file" do
     it "returns parsed data" do
       result = search.parse_file file_path
@@ -23,6 +24,30 @@ RSpec.describe "Search" do
         expect(search).to receive(:logger).and_return(double(error: ""))
         search.parse_file file_path
       end
+    end
+  end
+
+  describe "#search" do
+    let(:json_data) { search.parse_file file_path }
+    let(:key) { "_id" }
+    let(:value) { "4" }
+    it "searches item by key and value" do
+      result = search.search json_data, key, value
+      expect(result.is_a?(Array)).to eq true
+      expect(result[0][key].to_s).to eq value.to_s
+    end
+  end
+
+  describe "#start" do
+    let(:key) { "_id" }
+    let(:value) { "4" }
+    it "calls parse_file" do
+      expect(search).to receive(:parse_file).and_return([])
+      search.start file_path, key, value
+    end
+    it "calls search" do
+      expect(search).to receive(:search).and_return([])
+      search.start file_path, key, value
     end
   end
 
